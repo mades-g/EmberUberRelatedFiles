2c9f64813296b95a3118d9a0ad84d125be500289
Eudes G.G Duarte <eudes.duarte@infinity.co>
2019-01-26 17:10:41 +0000

    [refactor] - tidy with os walk
    ATM:
    	Works fine with test files
    	Routes and Controller only lookup to each respective test type

--- relatedFiles.py
+++ relatedFiles.py
@@ -4,135 +4,75 @@ import copy
 import sublime
 import sublime_plugin
 
+default_root = {
+  'app': '.js',
+  'tests': '-test.js'
+}
+
+default_files_config = {
+  'tests': default_root['tests'],
+  'templates': '.hbs',
+  'app': default_root['app']
+}
+
 class EmberUberRelatedFilesCommand(sublime_plugin.WindowCommand):
-
-  file_structure_regex_fix = "(?P<app_root>app)(?:/(?P<has_template>templates))?(?(1)/(.*?)/)|/(?P<test>tests)/(?P<test_type>.*?)/(?P<test_dir_ref>.*?)"
-  file_structure_regex = "/(?P<app_root>app)(?:/(?P<has_template>templates))?(?(1)/(.*?)/)|/(?P<test>tests)/(?P<test_type>.*?)/(?P<test_dir_ref>.*?)/"
-  struct_replacers = {
-  "tests": [ "unit", "integration", "acceptance" ],
-  "app": [ "templates", "routes", "components", "controllers", "adapters", "helpers", "instance-initializers", "mixins", "models", "serializers", "utils" ],
-  "templates": [ "" ]
-  }
-  file_type_ext = {
-    "tests": "-test.js",
-    "templates": ".hbs",
-    "app": ".js"
-  }
-
-  nooop = False
-
   def run(self):
-    self.nooop = False
-    def open_related_files(index):
-      if index >= 0 and self.nooop == False:
-        self.window.open_file(self.file_structure.get("related_files")[index])
-    ##
-
     self.current_view = self.window.active_view()
-
     self.og_path = self.current_view.file_name()
-    self.set_current_file_structure(self.og_path)
-    self.set_current_related_files()
 
-    related_files = self.file_structure.get("related_files")
+    file_config = self.create_types_config(self.og_path)
 
-    if related_files == None or len(related_files) == 0:
-      related_files = [ 'nooop' ]
-      self.nooop = True
+    related_files = self.find_related(**file_config)
 
-    self.window.show_quick_panel(self.file_type_creator(related_files), open_related_files)
+  # return {Dict}
+  def create_types_config(self, path):
+    file_config = {}
+    is_root_path = False
+    # Order regarding `tests` and `project type` matters
+    reg = ''
+    for file_type in default_files_config:
+      ext = default_files_config[file_type]
+      if ext in path:
+        if file_type == 'tests':
+          file_config['type'] = file_type
+          reg = '{type_config}{os_sep}\\w+{os_sep}(?P<section_type>\\w+){os_sep}(.*{os_sep})(?=(?P<file_name>.*){ext})'.format_map({'type_config': file_type, 'os_sep': os.sep, 'ext': ext})
+          break;
 
-  def set_current_file_structure(self, path):
-    self.file_structure = {}
+        file_config['type'] = 'app'
 
-    file_structure_search = re.search(self.file_structure_regex, path)
+    search_res = re.search(reg, path)
 
-    if file_structure_search is None:
-      file_structure_search = re.search(self.file_structure_regex_fix, path)
+    if search_res is not None:
+      file_config['path'] = '/' if is_root_path else search_res.groups()[1]
+      file_config['section_type'] = search_res.group('section_type')
+      file_config['name'] = search_res.group('file_name')
 
-    path_without_ext, ext = os.path.splitext(path)
+    return file_config
 
-    if file_structure_search and len(file_structure_search.groups()) > 0:
-      self.file_structure.setdefault("c_app_root", file_structure_search.group('app_root') or file_structure_search.group('test'))
-      self.file_structure.setdefault("c_file_type_reference_path", self.path_creator(file_structure_search.groups()))
-      self.file_structure.setdefault("c_sub_dir", file_structure_search.group("test_type") or file_structure_search.group(2))
-      self.file_structure.setdefault("c_sub_dir_type", file_structure_search.group("test_dir_ref") or file_structure_search.group(3))
-      self.file_structure.setdefault("c_ext", ext)
-      self.file_structure.setdefault("related_files", [])
-      self.file_structure.setdefault("c_path_helper", path_without_ext)
+  def find_related(self, **file_config):
+    related = []
+    path = file_config['path']
+    section_type = file_config['section_type']
+    file_name = file_config['name']
+    project_root = re.sub('(.*{root}{os_sep}).*'.format_map({'os_sep': os.sep, 'root': file_config['type']}), r'\1', self.og_path)
 
-      if self.file_structure.get("c_app_root") == "tests":
-        self.file_structure["c_path_helper"] = path_without_ext.replace("-test", '')
+    for root in default_root:
+      project_root = re.sub('(\\w+){os_sep}$'.format_map({'os_sep': os.sep}), root + os.sep, project_root)
 
-  def set_current_related_files(self):
-    if self.file_structure.get("c_file_type_reference_path") == None:
-      return
+      for root_path, dirs, files in os.walk(project_root):
 
-    split_path = self.file_structure.get("c_file_type_reference_path").split(os.sep)
-    _path = ""
-    replaces_shadow = copy.deepcopy(self.struct_replacers)
-
-    if len(split_path) == 3:
-      for root_dir, sub_dir in replaces_shadow.items():
-        for index in range(len(sub_dir)):
-          path_helper = self.file_structure.get("c_path_helper") + self.file_type_ext.get(root_dir)
-          if root_dir == "templates":
-            # hum that hard coded app tho
-            _path = path_helper.replace(self.file_structure.get("c_file_type_reference_path"), "app" + os.sep + root_dir + os.sep + sub_dir[index] + os.sep).replace("//", "/")
-            if self.is_valid_path(_path) == False and self.file_structure.get("c_sub_dir_type") is not None:
-              _path = path_helper.replace(self.file_structure.get("c_file_type_reference_path"), "app" + os.sep + root_dir + os.sep + self.file_structure.get("c_sub_dir_type") + os.sep).replace("//", "/")
-
-          elif root_dir == "tests" and sub_dir[index] != "acceptance" and self.file_structure.get("c_sub_dir_type") is not None:
-              _path = path_helper.replace(self.file_structure.get("c_file_type_reference_path"), root_dir + os.sep + sub_dir[index] + os.sep + self.file_structure.get("c_sub_dir_type") + os.sep)
+        for dir in dirs:
+          if dir != 'acceptance' and root == 'tests':
+            lookup = project_root + dir + os.sep + section_type + os.sep + path
           else:
-            _path = path_helper.replace(self.file_structure.get("c_file_type_reference_path"), root_dir + os.sep + sub_dir[index] + os.sep)
+            lookup = project_root + dir + os.sep + path
+          if os.path.exists(lookup):
+            if 'templates' in lookup:
+              found = lookup + file_name + default_files_config['templates']
+            else:
+              found = lookup + file_name + default_root[root]
 
-          if self.is_valid_path(_path) and self.og_path != _path:
-            self.file_structure.get("related_files").append(_path)
+            if os.path.isfile(found) and found != self.og_path and found not in related:
+              related.append(found)
 
-    elif len(split_path) == 4:
-      _path_helper = self.file_structure.get("c_path_helper") + self.file_type_ext.get("app")
-      if self.file_structure.get("c_sub_dir_type") not in self.struct_replacers.get("app"):
-        # since its not in could be routes or controllers
-        _path_helper = _path_helper.replace(self.file_structure.get("c_file_type_reference_path"), "app" + os.sep + "routes" + os.sep + self.file_structure.get("c_sub_dir_type") + os.sep)
-
-        if self.is_valid_path(_path_helper) == False:
-          _path_helper = _path_helper.replace(self.file_structure.get("c_file_type_reference_path"), "app" + os.sep + "controllers" + os.sep + self.file_structure.get("c_sub_dir_type") + os.sep)
-      else:
-        _path_helper = _path_helper.replace(self.file_structure.get("c_file_type_reference_path"), "app" + os.sep + self.file_structure.get("c_sub_dir_type") + os.sep)
-      if self.is_valid_path(_path_helper):
-        self.set_current_file_structure(_path_helper)
-        self.set_current_related_files()
-
-  @staticmethod
-  def path_creator(regex_res_group):
-    path = ""
-    for value in regex_res_group:
-      if value is not None and value is not '':
-        path += value + os.sep
-    return path
-
-  @staticmethod
-  def file_type_creator(_list_of_files):
-    _ref = []
-    _x = ""
-
-    for value in _list_of_files:
-      res = re.search("(app/(.*?)/.*)", value)
-      if res is not None:
-        _r = res.groups()
-        _x = _r[1].capitalize()[:-1] + ' -> ' + _r[0]
-
-      if "/tests/" in value:
-        res = re.search("(tests/(.*?)/.*)", value)
-        if res is not None:
-          _r = res.groups()
-          _x = 'Test: ' + _r[1].capitalize() + ' -> ' + _r[0]
-      if _x != "":
-        _ref.append(_x)
-    return _ref
-
-  @staticmethod
-  def is_valid_path(path):
-    return os.path.exists(path)
-
+    return related
