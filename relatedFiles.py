f740e1c29060bcefb592dfe50db066852bfbc19f
Eudes G.G Duarte <eudes.duarte@infinity.co>
2019-01-27 20:49:32 +0000

    find related

--- relatedFiles.py
+++ relatedFiles.py
@@ -21,6 +21,8 @@ class EmberUberRelatedFilesCommand(sublime_plugin.WindowCommand):
 
     file_config = self.create_types_config()
 
+    self.find_related(**file_config)
+
   def get_project_type(self, path):
     project_root = 'app'
 
@@ -35,14 +37,14 @@ class EmberUberRelatedFilesCommand(sublime_plugin.WindowCommand):
   def create_types_config(self):
 
     reg = ''
+    file_config = {}
     path = self.og_path
     is_root_path = False
     project_type = self.get_project_type(path)
-    project_root = re.sub('(.*{root}{sep}).*'.format_map({'sep': os.sep, 'root': project_type}), r'\1', path)
-    file_config = {}
+    self.project_root = re.sub('(.*{root}{sep}).*'.format_map({'sep': os.sep, 'root': project_type}), r'\1', path)
 
     ext = default_files_config.get(project_type, '.js')
-    reg = '{project_root}(.*)(?={ext})'.format_map({'project_root': project_root, 'ext': ext})
+    reg = '{project_root}(.*)(?={ext})'.format_map({'project_root': self.project_root, 'ext': ext})
 
     sub_paths_info_res = re.search(reg, path)
 
@@ -57,7 +59,7 @@ class EmberUberRelatedFilesCommand(sublime_plugin.WindowCommand):
         file_path_res = re.search('.*{sep}{type}{sep}(.*){sep}{name}'.format_map({'sep': os.sep, 'type': file_id, 'name': file_config['name']}), path)
 
         if file_path_res is not None:
-          file_config['path'] = file_path_res.group(0)
+          file_config['path'] = file_path_res.group(1)
       else:
         file_config['section_type'] = sub_paths_info_res.groups()[0].split(os.sep)[0]
 
@@ -66,4 +68,21 @@ class EmberUberRelatedFilesCommand(sublime_plugin.WindowCommand):
       if file_path_res is not None:
           file_config['path'] = file_path_res.group(1)
 
+      if project_type == 'templates':
+        file_config['type'] = 'app'
+      else:
+        file_config['type'] = project_type
       file_config.setdefault('path', os.sep)
+
+    return  file_config
+
+  def find_related(self, **file_config):
+    related = []
+    path = file_config['path']
+    section_type = file_config['section_type']
+    file_name = file_config['name']
+
+    for root in default_root:
+      project_root = re.sub('(.*{root}{os_sep}).*'.format_map({'os_sep': os.sep, 'root': file_config['type']}), r'\1', self.og_path)
+
+    return related
